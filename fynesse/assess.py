096a5622a1721f25c2d01a6f85f0eb087746010a
Joel Robinson <jr919@cam.ac.uk>
2024-11-27 10:44:59 +0000

    add get_df_from_sql method to assess

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -2,6 +2,7 @@ import pandas as pd
 from .config import *
 import osmnx as ox
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 from . import access
 
 """These are the types of import we might expect in this file
@@ -14,24 +15,10 @@ import sklearn.feature_extraction"""
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
-
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
-
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
-
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
-
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
-
+def get_df_from_sql(table_name, username, password, url):
+  engine = create_engine(f"mysql+pymysql://{username}:{password}@{url}:{3306}/ads_2024")
+  query = 'SELECT * FROM ' + table_name 
+  return pd.read_sql_query(query, engine)
 
 def get_box(latitude, longitude, length):
   box_height = 0.018*length
